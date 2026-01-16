4d3bb17d1e04821012c6685ffc01f7572f330386
Andrei05231 <carare.andrei57@gmail.com>
2026-01-16 15:28:37 +0200

    ajdust response check to match new data structure

--- api/odoo_client.py
+++ api/odoo_client.py
@@ -37,11 +37,12 @@ class OdooClient:
 
             if response.status_code in [200,201,202]:
                 logger.info(f"Sucesfully sent computers to Odoo")
-                if response.json()['summary']['not_found']>0:
+                logger.info(f"response:{response.json()}")
+                if response.json()['computer_summary']['not_found']>0:
                     logger.warning(f'Some computers were not found {response.json()["summary"]}')
                 return True
             else:
-                logger.error(f"Could not send computers to Odoo: {e}")
+                logger.error(f"Could not send computers to Odoo: { response.json() }")
                 return False
 
         except Exception as e :
